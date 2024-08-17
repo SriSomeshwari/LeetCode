204c16de93f6c9c1c2c81ea2c4b8a5deca752513
SOMESHWARI <99348894+SriSomeshwari@users.noreply.github.com>
2024-08-17 11:45:23 +0530

    Time: 5 ms (98.71%) | Memory: 42.8 MB (91.85%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,51 +1,51 @@
 import java.util.Arrays;
 import java.util.Comparator;
-import java.util.Scanner;
 
-class Solution {
+public class Solution {
     public String largestNumber(int[] nums) {
-     
-        String[] strNums = new String[nums.length];
+        // Convert integers to strings
+        String[] numStrs = new String[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            strNums[i] = Integer.toString(nums[i]);
+            numStrs[i] = Integer.toString(nums[i]);
         }
 
-    
-        Arrays.sort(strNums, new Comparator<String>() {
+        // Sort strings using a custom comparator
+        Arrays.sort(numStrs, new Comparator<String>() {
             @Override
-            public int compare(String s1, String s2) {
-               
-                return (s2 + s1).compareTo(s1 + s2);
+            public int compare(String num1, String num2) {
+                // Compare concatenated results
+                return (num2 + num1).compareTo(num1 + num2);
             }
         });
 
-        if (strNums[0].equals("0")) {
+        // Edge case: if the largest number is "0", return "0"
+        if (numStrs[0].equals("0")) {
             return "0";
         }
 
-     
-        StringBuilder largestNum = new StringBuilder();
-        for (String num : strNums) {
-            largestNum.append(num);
+        // Concatenate all numbers to form the largest number
+        StringBuilder largestNumber = new StringBuilder();
+        for (String numStr : numStrs) {
+            largestNumber.append(numStr);
         }
 
-        return largestNum.toString();
+        return largestNumber.toString();
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-  
-int n=scanner.nextInt();
-        int[] nums = new int[n];
-        
-        for (int i = 0; i < n; i++) {
-            nums[i] = scanner.nextInt();
-        }
-
+        // Test the Solution class
         Solution solution = new Solution();
-        System.out.println("Largest Number: " + solution.largestNumber(nums));
-
-        scanner.close();
+        
+        // Test case 1
+        int[] nums1 = {10, 2};
+        System.out.println(solution.largestNumber(nums1));  // Output: "210"
+        
+        // Test case 2
+        int[] nums2 = {3, 30, 34, 5, 9};
+        System.out.println(solution.largestNumber(nums2));  // Output: "9534330"
+        
+        // Test case 3
+        int[] nums3 = {0, 0};
+        System.out.println(solution.largestNumber(nums3));  // Output: "0"
     }
 }
