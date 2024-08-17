b6f78b86804850e84617bb1d4d0e3a32ea44137b
SOMESHWARI <99348894+SriSomeshwari@users.noreply.github.com>
2024-08-17 10:29:46 +0530

    Time: 5 ms (98.71%) | Memory: 43.2 MB (46.78%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -35,11 +35,10 @@ class Solution {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Enter the number of elements: ");
-        int n = scanner.nextInt();
-
+  
+int n=scanner.nextInt();
         int[] nums = new int[n];
-        System.out.println("Enter the elements:");
+        
         for (int i = 0; i < n; i++) {
             nums[i] = scanner.nextInt();
         }
