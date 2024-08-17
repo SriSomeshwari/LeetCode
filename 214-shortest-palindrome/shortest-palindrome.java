1ab7f80677ecc5432dd26dc6559c8407f1a8e012
SOMESHWARI <99348894+SriSomeshwari@users.noreply.github.com>
2024-08-17 14:50:58 +0530

    Time: 7 ms (74.46%) | Memory: 44.9 MB (42.49%) - LeetSync

--- 214-shortest-palindrome/shortest-palindrome.java
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -1,10 +1,10 @@
 class Solution {
     public String shortestPalindrome(String s) {
         
-        String rev = new StringBuilder(s).reverse().toString();
-        String l=s + "#" + rev;
+        String rev=new StringBuilder(s).reverse().toString();
+        String l=s+"#"+rev;
         int[] lps=lpsArray(l);
-        String add=rev.substring(0, rev.length() - lps[lps.length - 1]);
+        String add=rev.substring(0,rev.length()-lps[lps.length-1]);
         return add+s;
     }
 
@@ -12,18 +12,19 @@ class Solution {
     private int[] lpsArray(String s) {
         int n=s.length();
         int[] lps=new int[n];
-        int length = 0;
+        int length =0;
         int i=1;
 
-        while (i<n) {
-            if (s.charAt(i) == s.charAt(length)) {
+        while(i<n){
+            if(s.charAt(i)==s.charAt(length)) {
                 length++;
-                lps[i] = length;
+                lps[i]=length;
                 i++;
-            } else {
-                if (length != 0) {
-                    length = lps[length - 1];
-                } else {
+            } else{
+                if(length!=0) {
+                    length=lps[length-1];
+                } 
+                else{
                     lps[i]=0;
                     i++;
                 }
@@ -32,3 +33,4 @@ class Solution {
          return lps;
     }
 }
+
