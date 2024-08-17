090da0aa3ef66ff04facaabfe39dbff588dc97c1
SOMESHWARI <99348894+SriSomeshwari@users.noreply.github.com>
2024-08-17 11:55:24 +0530

    Time: 12 ms (8.44%) | Memory: 44.9 MB (5.32%) - LeetSync

--- 179-largest-number/largest-number.java
+++ 179-largest-number/largest-number.java
@@ -1,51 +1,29 @@
-import java.util.Arrays;
-import java.util.Comparator;
-
-public class Solution {
+class Solution {
     public String largestNumber(int[] nums) {
-        // Convert integers to strings
-        String[] numStrs = new String[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            numStrs[i] = Integer.toString(nums[i]);
+        String[] str=new String[nums.length];
+        for(int i=0;i<str.length;i++){
+        str[i]=nums[i]+"";
         }
-
-        // Sort strings using a custom comparator
-        Arrays.sort(numStrs, new Comparator<String>() {
-            @Override
-            public int compare(String num1, String num2) {
-                // Compare concatenated results
-                return (num2 + num1).compareTo(num1 + num2);
+        for (int i = 0; i < str.length - 1; i++) {
+            for (int j = 0; j < str.length - i - 1; j++) {
+                String s1 = str[j] + str[j + 1];
+                String s2 = str[j + 1] + str[j];
+                if (s1.compareTo(s2) < 0) {
+                    String temp = str[j];
+                    str[j] = str[j + 1];
+                    str[j + 1] = temp;
+                } 
             }
-        });
-
-        // Edge case: if the largest number is "0", return "0"
-        if (numStrs[0].equals("0")) {
+        }
+        if(str[0].equals("0")){
             return "0";
         }
-
-        // Concatenate all numbers to form the largest number
-        StringBuilder largestNumber = new StringBuilder();
-        for (String numStr : numStrs) {
-            largestNumber.append(numStr);
+        StringBuilder sb=new StringBuilder();
+        for(String n:str){
+            sb.append(n);
         }
 
-        return largestNumber.toString();
-    }
-
-    public static void main(String[] args) {
-        // Test the Solution class
-        Solution solution = new Solution();
+      return sb.toString();
         
-        // Test case 1
-        int[] nums1 = {10, 2};
-        System.out.println(solution.largestNumber(nums1));  // Output: "210"
-        
-        // Test case 2
-        int[] nums2 = {3, 30, 34, 5, 9};
-        System.out.println(solution.largestNumber(nums2));  // Output: "9534330"
-        
-        // Test case 3
-        int[] nums3 = {0, 0};
-        System.out.println(solution.largestNumber(nums3));  // Output: "0"
     }
 }
