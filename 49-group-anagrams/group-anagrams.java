ba1243b543602744aa9e7b4be375cbc0144a7f72
SOMESHWARI <99348894+SriSomeshwari@users.noreply.github.com>
2024-08-18 14:25:55 +0530

    Time: 1244 ms (5.07%) | Memory: 47.4 MB (89.18%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,27 +1,64 @@
-import java.util.*;
-
-public class Solution {
+import java.util.Arrays;
+class Solution {
+    public boolean isAnagram(String a,String b){
+        if(a.length()!=b.length())
+         return false;
+        char a1[]=a.toCharArray();
+        char b1[]=b.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(b1);
+        String s1=new String(a1);
+        String s2=new String(b1);
+        if(s1.equals(s2))
+         return true;
+        else
+         return false;
+         
+    }
     public List<List<String>> groupAnagrams(String[] strs) {
-        List<List<String>> result=new ArrayList<>();
-        List<String> sortedStrings=new ArrayList<>();
+       List<List<String>> res =new ArrayList<>();
+       List<String>l=new ArrayList<>();
 
-        for(String str:strs) {
-     
-            char[] chars=str.toCharArray();
-            Arrays.sort(chars);
-            String sortedStr=new String(chars);
-            
-      
-            int index=sortedStrings.indexOf(sortedStr);
-            if(index!=-1){
-              result.get(index).add(str);
-            }else{
-                List<String> newGroup=new ArrayList<>();
-                newGroup.add(str);
-                result.add(newGroup);
-                sortedStrings.add(sortedStr);
-            }
+       if(strs.length==0){
+        l.add("");
+         res.add(l);
+         return res;
+       }
+       if(strs.length==1)
+       {
+        l.add(strs[0]);
+        res.add(l);
+        return res;
+       }
+       int n=strs.length;
+       int visited[]=new int[n];
+       for(int i=0;i<n;i++){
+         List<String> li=new ArrayList<>();
+          int c=0;
+        for(int j=i+1;j<n;j++){
+           
+           if(isAnagram(strs[i],strs[j])){
+            c=1;
+            if(visited[i]==0)
+             { li.add(strs[i]);
+               
+             }
+             if(visited[j]==0){
+                li.add(strs[j]);
+             }
+            visited[i]=1;
+            visited[j]=1;
+           }
+          
+         
         }
-       return result;
+         if(c==0){
+            if(visited[i]==0)
+              li.add(strs[i]);
+           }
+           if(!li.isEmpty())
+             res.add(li);
+       } 
+       return res;
     }
 }
